ff74e283510622c2059109b9785fcebdc55c93e6
CGFDOO <mazenahmedghaith598@gmail.com>
2026-02-13 12:46:49 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,13 @@
+module.exports = {
 
+TOKEN: "PUT_TOKEN_HERE",
+
+CATEGORY_ID: "حط ايدي الكاتيجوري",
+
+STAFF_ROLE: "ايدي رتبة الادارة",
+
+HIGH_ROLE: "ايدي الادارة العليا",
+
+COLOR: 0x000000
+
+}
