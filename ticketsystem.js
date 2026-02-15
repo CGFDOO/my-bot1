15d4e22d94207c6681eee00b8af55ba3c089f16b
CGFDOO <mazenahmedghaith598@gmail.com>
2026-02-15 15:19:36 -0800

    Update ticketsystem.js

--- ticketsystem.js
+++ ticketsystem.js
@@ -344,5 +344,8 @@ if (client.cooldowns.has(interaction.user.id)) {
 client.cooldowns.add(interaction.user.id);
     setTimeout(() => client.cooldowns.delete(interaction.user.id), 2000); 
 
-}
-};
+}); // دي قفلة البوت
+
+console.log('✅ Ticket System Ready');
+
+}; // دي قفلة الملف (مهمة جداً)
