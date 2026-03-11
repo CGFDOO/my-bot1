c87d503a7047dc78f3fe7f9399fd51fb8fc77e64
CGFDOO <mazenahmedghaith598@gmail.com>
2026-03-10 18:01:08 -0700

    Update Level.js

--- models/Level.js
+++ models/Level.js
@@ -4,20 +4,16 @@ const levelSchema = new mongoose.Schema({
     guildId: { type: String, required: true },
     userId: { type: String, required: true },
     
-    // إجمالي المستوى والخبرة
     level: { type: Number, default: 0 },
     xp: { type: Number, default: 0 },
     
-    // تفصيل الخبرة (نصي وصوتي) زي برو بوت
     textXp: { type: Number, default: 0 },
     voiceXp: { type: Number, default: 0 },
     
-    // أنظمة التوب (تتريست حسب الوقت)
     dailyXp: { type: Number, default: 0 },
     weeklyXp: { type: Number, default: 0 },
     monthlyXp: { type: Number, default: 0 },
     
-    // لتفادي السبام (Cooldown)
     lastMessageTime: { type: Date, default: null }
 });
 
